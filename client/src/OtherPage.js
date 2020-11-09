a55ef4b340d453610eeefa76f1d3a76bec0ba03a
m_poghosyan <Mikayel.Poghosyan@mindgeek.com>
2020-11-08 23:44:57 -0500

    added client files

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-dom';
+
+export default () => {
+    return (
+        <div>
+            I'm some other page!
+            <Link to='/'>Go Back Home</Link>
+        </div>
+    );
+};
